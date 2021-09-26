0dcc63d11acc12736f1ad007cc80fac10a523095
Amir Mujkic <mujkicc@gmail.com>
2021-09-27 00:10:38 +0200

    Small code change

--- ex07/src/index.js
+++ ex07/src/index.js
@@ -1,11 +1,11 @@
 // Only change code below this line
 var sum = 0;
 function addThree() {
-    sum += 3;
+    sum = sum + 3;
     console.log("addThree:", sum);
 }
 function addFive() {
-    sum += 5;
+    sum = sum + 5;
     console.log("addFive:", sum)
 }
 addThree();
